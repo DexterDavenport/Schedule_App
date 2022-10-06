d962d6aab0442f3825ea7b4ef6a621f1b846e189
dexterdavenport <dexter.davenport1@gmail.com>
2022-10-06 15:01:41 -0600

    Slide calandar

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:scheduler/components/calandar.dart';
 
 class CalendarPage extends StatelessWidget {
+  const CalendarPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calendar Page'),
+      ),
+      body: Column(
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          const Calendar(),
+        ],
+      ),
+    );
   }
-  
 }
